c6b330d038a0369f76c823514a67787ec348aa70
Alex Nguyen <hoangminh98@gmail.com>
2022-01-03 22:38:38 -0800

    Add favicons, consistent tab size

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" href="/logo192.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
